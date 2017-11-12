80ae0ac4f3c1cf1c80aefc38b05930fa870966f4
Taras19 <drobnitskytaras19@gmail.com>
2017-11-12 16:30:35 +0300

    Add files via upload

--- js/myjs.js
+++ js/myjs.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
 	/* висота сторінки */
-  let scrollHeight = Math.max(
+  var scrollHeight = Math.max(
     document.body.scrollHeight, document.documentElement.scrollHeight,
     document.body.offsetHeight, document.documentElement.offsetHeight,
     document.body.clientHeight, document.documentElement.clientHeight
   );
 
   /* висота екрану */
-  let windowHeight = document.documentElement.clientHeight;
+  var windowHeight = document.documentElement.clientHeight;
 
   /* поточна прокрутка*/
-  let scrollСurrent = document.body.scrollTop || document.documentElement.scrollTop;
+  var scrollСurrent = document.body.scrollTop || document.documentElement.scrollTop;
 
   /* кнопка меню */
-  let menuIcon = document.querySelector(".menu-icon");
-  let menu = document.querySelector(".menu");
+  var menuIcon = document.querySelector(".menu-icon");
+  var menu = document.querySelector(".menu");
     menuIcon.addEventListener("click",function(){
       menuIcon.classList.toggle("menu-icon--open");
       menu.classList.toggle("menu--open");
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
   /*перехід меню з position:static на fixed*/
   window.addEventListener("scroll",function(){
-    let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
       if(scrollTop > 30){
         menu.classList.add("menu-fixed");
         }
@@ -47,9 +47,9 @@ document.addEventListener("DOMContentLoaded", function(){
         }
 
       /* перемикання .active в пунктах меню*/
-    let containerPageAll = document.querySelectorAll(".container-page-js");
-      for (let i=0; i < containerPageAll.length; i++){
-        let coorContainerPage=containerPageAll[i].getBoundingClientRect();
+    var containerPageAll = document.querySelectorAll(".container-page-js");
+      for (var i=0; i < containerPageAll.length; i++){
+        var coorContainerPage=containerPageAll[i].getBoundingClientRect();
           if(coorContainerPage.top < windowHeight/2 && coorContainerPage.bottom > windowHeight/2){
             document.querySelector(".menu li.active").classList.remove("active");
             document.querySelectorAll(".menu li")[i].classList.add("active");
@@ -61,19 +61,19 @@ document.addEventListener("DOMContentLoaded", function(){
       }
   });
   /* подія для пунктів меню */
-  let menuLinkAll = document.querySelectorAll(".menu li a,.double-down");
-  let step =30;
-  for(let i = 0; i < menuLinkAll.length; i++){
+  var menuLinkAll = document.querySelectorAll(".menu li a,.double-down");
+  var step =30;
+  for(var i = 0; i < menuLinkAll.length; i++){
     menuLinkAll[i].addEventListener("click",function(event){
       menuIcon.classList.remove("menu-icon--open");
       menu.classList.remove("menu--open");
       event.preventDefault();
-      let href = this.getAttribute("href");
-      let coorHref = document.querySelector(href).getBoundingClientRect();
+      var href = this.getAttribute("href");
+      var coorHref = document.querySelector(href).getBoundingClientRect();
       if (coorHref.top > 0){
         function scrollDown(){
-          let scrollСurrent = window.pageYOffset || document.documentElement.scrollTop;
-          let coorHref = document.querySelector(href).getBoundingClientRect();
+          var scrollСurrent = window.pageYOffset || document.documentElement.scrollTop;
+          var coorHref = document.querySelector(href).getBoundingClientRect();
           if( 0 < coorHref.top && scrollСurrent !== scrollHeight - windowHeight){
             requestAnimationFrame(scrollDown);
             window.scrollTo(0,scrollСurrent+step);
@@ -95,12 +95,14 @@ document.addEventListener("DOMContentLoaded", function(){
       }
       else{
         function scrollUp(){
-          let scrollСurrent = window.pageYOffset || document.documentElement.scrollTop;
-          let coorHref = document.querySelector(href).getBoundingClientRect();
+          var scrollСurrent = window.pageYOffset || document.documentElement.scrollTop;
+          var coorHref = document.querySelector(href).getBoundingClientRect();
           if(0 > coorHref.top){
             requestAnimationFrame(scrollUp);
             window.scrollTo(0,scrollСurrent-step);
-                
+                //console.log(coorHref.top+" "+"coorHref.top");
+                //console.log(scrollСurrent-step);
+                //console.log(scrollСurrent);
           }
           else{
             window.scrollTo(0,scrollСurrent+coorHref.top);
@@ -112,10 +114,49 @@ document.addEventListener("DOMContentLoaded", function(){
       }
     });
   }
-  let portfolioFotoAll = document.querySelectorAll(".portfolio-foto");
-  for (let i = 0; i < portfolioFotoAll.length; i++){
-  	if ('ontouchstart' in window) {
-  	  portfolioFotoAll[i].classList.add("portfolio-foto__touch");
-  	}
+
+  /**/
+  let portfolioContainerAll = document.querySelectorAll(".portfolio-container");
+  //console.log(portfolioContainerAll.length);
+  let listSrc = [
+    ["img/world_around_us/page-1.jpg",
+    "img/world_around_us/page-2.jpg",
+    "img/world_around_us/page-3.jpg"],
+    ["img/the_band/page-1.jpg",
+    "img/the_band/page-2.jpg",
+    "img/the_band/page-3.jpg"],
+    ["img/elegant/page-1.jpg",
+    "img/elegant/page-2.jpg",
+    "img/elegant/page-3.jpg"],
+    ["img/eclipse/page-1.jpg",
+    "img/eclipse/page-2.jpg",
+    "img/eclipse/page-3.jpg"]
+    ];
+  let listAlt = [
+    ["Home page",
+    "About myself",
+    "Portfolio"],
+    ["Home page",
+    "Meet our band",
+    "Discography"],
+    ["Home page",
+    "Show your amazing work",
+    "Contact us",],
+    ["Home page",
+    "Our design guidelines",
+    "Push yourself your designs"]
+  ];
+  for (let i = 0; i < portfolioContainerAll.length;i++){
+    //console.log(i+" i");
+    //console.log(listSrc.length+" listSrc.length");
+    for(let k =1; k<listSrc[i].length;k++){
+      //console.log(listSrc[i].length+" listSrc[i].length");
+      //console.log(k+" k");
+      let portfolioFoto = document.createElement("img");
+      portfolioFoto.classList.add("portfolio-foto");
+      portfolioFoto.src=listSrc[i][k];
+      portfolioFoto.alt=listAlt[i][k];
+      portfolioContainerAll[i].appendChild(portfolioFoto );
+    }
   }
 });
