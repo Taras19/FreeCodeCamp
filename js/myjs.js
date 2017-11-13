31cc9f737b697677ca6a1c67a9c97d548d53277c
Taras19 <drobnitskytaras19@gmail.com>
2017-11-13 15:05:43 +0300

    зміна події виконання load
    document.addEventListener(DOMContentLoaded на load) (хочу пришвидшити завантаження)

--- js/myjs.js
+++ js/myjs.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("load", function(){
   /**/
   let portfolioContainerAll = document.querySelectorAll(".portfolio-container");
   let listSrc = [
